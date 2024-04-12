051facb7e691a52f0ed74678faa942a71d4efcec
Shahil <guptashahil0011@gmail.com>
2024-04-12 17:37:47 +0530

    my second commit

--- project_a/src/App.js
+++ project_a/src/App.js
@@ -1,12 +1,41 @@
 
 import './App.css';
+import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Homepage from './Components/Homepage';
+import  FindJob1 from './Components/FindJob1';
+import Login from './Components/Login';
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 function App() {
   return (
    <>
-   <Header/>
-   <Homepage/>
+  <Router>
+      <div className="App">
+        <Header />
+        
+        <main>
+          <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/findjob" element={<FindJob1/>}/>
+          <Route path="/login" element={<Login/>}/>
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
+    {/* <BrowserRouter>
+    <Routes>
+    <Header/>
+      <Switch>
+      <Route index element={<Homepage/>}/>
+      <Route path="/findjob" element={<FindJob1/>}/>
+     </Switch>
+     <Footer/>
+    </Routes>
+   
+    </BrowserRouter> */}
+  
    </>
   );
 }
